4d51dbdef00d3a430d7c661022f1b12d00f9d050
Pavel Kovalyov <pavlo.kovalyov@gmail.com>
2016-11-18 11:16:32 +0300

    Committed some changes
    http://whatthecommit.com/231cfca1ead59da97a19d07321b8ccf1

--- app/main.ts
+++ app/main.ts
@@ -4,10 +4,23 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @Component({
   selector: 'racing-app',
-  template: '<h1>{{heading}}</h1>'
+  template: `
+    <h1>{{ heading }}</h1>
+    <h2>{{ race.name }}</h2>
+    <p>{{ race.date }}</p>
+    <p>{{ race.about }}</p>
+  `
 })
 class AppComponent {
-  heading = "Ultra Racing Schedule"
+  heading = 'Ultra Racing Schedule'
+
+  race = {
+    'id': 1,
+    'name': 'Daytona Thunderdome',
+    'date': new Date('2512-01-04T14:00:00'),
+    'about': 'Race through the ruins of an ancient Florida battle arena.',
+    'entryFee': 3200
+  }
 }
 
 @NgModule({
