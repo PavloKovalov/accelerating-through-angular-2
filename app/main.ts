c1ef1781f1eb339c5a0316bd83851ade5fdc34f0
Pavel Kovalyov <pavlo.kovalyov@gmail.com>
2016-11-19 11:06:07 +0300

    did everything
    http://whatthecommit.com/44acca093ddb762c6f532ffc45f9376e

--- app/main.ts
+++ app/main.ts
@@ -6,21 +6,37 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
   selector: 'racing-app',
   template: `
     <h1>{{ heading }}</h1>
-    <h2>{{ race.name }}</h2>
-    <p>{{ race.date }}</p>
-    <p>{{ race.about }}</p>
+    <ul>
+      <li *ngFor="let race of races">
+        <h2>{{ race.name }}</h2>
+        <p>{{ race.date }}</p>
+        <p>{{ race.about }}</p>
+      </li>
+    </ul>
   `
 })
 class AppComponent {
-  heading = 'Ultra Racing Schedule'
+  heading = 'Ultra Racing Schedule';
 
-  race = {
-    'id': 1,
-    'name': 'Daytona Thunderdome',
-    'date': new Date('2512-01-04T14:00:00'),
-    'about': 'Race through the ruins of an ancient Florida battle arena.',
-    'entryFee': 3200
-  }
+  races = [{
+    "id": 1,
+    "name": "Daytona Thunderdome",
+    "date": new Date('2512-01-04T14:00:00'),
+    "about": "Race through the ruins of an ancient Florida battle arena.",
+    "entryFee": 3200
+  }, {
+    "id": 2,
+    "name": "San Francisco Ruins",
+    "date": new Date('2512-07-03T20:00:00'),
+    "about": "Drift down the streets of a city almost sunk under the ocean.",
+    "entryFee": 4700
+  }, {
+    "id": 3,
+    "name": "New York City Skyline",
+    "date": new Date('2512-07-12T21:00:00'),
+    "about": "Fly between buildings in the electronic sky.",
+    "entryFee": 4300
+  }];
 }
 
 @NgModule({
